c852162a5a031b6ebeee49da4f2dd0c5b1e90248
Vaishnavi Shinde <78314784+Vaishnavi2603@users.noreply.github.com>
2021-02-15 07:11:06 -0800

    Add files via upload

---
+++ Syrup.java
@@ -0,0 +1,11 @@
+package Inheritance_Abstraction2;
+
+public class Syrup extends Medicine {
+
+	void displayLabel() {
+		System.out.println("SYRUP:");
+		System.out.println("Keep out of reach of Children");
+
+	}
+
+}
---
+++ Tablet.java
@@ -0,0 +1,13 @@
+package Inheritance_Abstraction2;
+
+public class Tablet extends Medicine {
+
+	@Override
+	void displayLabel() {
+		System.out.println("TABLET:");
+		System.out.println("Store in a cool dry place");
+
+	}
+
+}
+
